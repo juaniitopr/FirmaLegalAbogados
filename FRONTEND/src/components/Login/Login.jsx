2ae58601e4d7c4a9cc0e5d15671219fd10cec7a4
juaniito.pr <168869098+juaniitopr@users.noreply.github.com>
2025-06-12 01:56:23 +0000

    Correcion Login

--- FRONTEND/src/components/Login/Login.jsx
+++ FRONTEND/src/components/Login/Login.jsx
@@ -34,41 +34,46 @@ const Login = ({ isOpen, closeLogin }) => {
     });
   };
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setLoading(true);
+const handleLogin = async (e) => {
+  e.preventDefault();
+  setLoading(true);
 
-    try {
-      const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/autenticacion`,
-        formValues, {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/autenticacion`,
+      formValues,
+      {
         headers: { "Content-Type": "application/json" },
-      });
-
-      if (!response.data.token) {
-        throw new Error('No se recibió un token');
       }
+    );
 
-      const { token } = response.data;
-      const payload = JSON.parse(atob(token.split(".")[1]));
-
-      login(token, payload.role);
-
-      const roleRoutes = {
-        asistente: "/page-principal",
-        abogado: "/abogado",
-        cliente: "/d_cliente"
-      };
-      const userRoute = roleRoutes[payload.nombre_rol] || "/homepage";
-      navigate(userRoute, { replace: true });
-    } catch (error) {
-      console.error(error);
-      const errorMessage = error.response?.data?.message || 'Error al iniciar sesión. Intenta nuevamente.';
-      alert(errorMessage);
-    } finally {
-      setLoading(false);
+    if (!response.data.token) {
+      throw new Error('No se recibió un token');
     }
-  };
+
+    const { token } = response.data;
+    const payload = JSON.parse(atob(token.split(".")[1]));
+
+    login(token, payload.role);
+
+    const roleRoutes = {
+      asistente: "/page-principal",
+      abogado: "/abogado",
+      cliente: "/d_cliente"
+    };
+
+    const userRoute = roleRoutes[payload.nombre_rol] || "/homepage";
+    navigate(userRoute, { replace: true });
+
+  } catch (error) {
+    console.error(error);
+    const errorMessage = error.response?.data?.message || 'Error al iniciar sesión. Intenta nuevamente.';
+    alert(errorMessage);
+  } finally {
+    setLoading(false);
+  }
+};
+
 
   const closeAndClearForm = () => {
     closeLogin();
